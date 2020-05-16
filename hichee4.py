53f56ff1b1c791d65be159d7bae286d4c68a6a6f
naranundra <64673333+naranundra@users.noreply.github.com>
2020-05-16 11:06:39 +0800

    while ашиглах

---
+++ hichee4.py
@@ -0,0 +1,16 @@
+# bool > true, false утга авдаг өөр утга авдаггүй
+a = 123
+b = 568
+C = bool a > b:
+print(c)
+
+
+
+# 1-n тооны үржвэр олох
+n = int(input("too: "))
+vrjwer = 1
+while n>0:
+    vrjwer = vrjwer*n
+    n-=1 
+print(vrjwer)
+
---
+++ hicheel2.py
@@ -0,0 +1,8 @@
+# 1-n тооны нийлбэх олох
+n = int(input("too: "))
+niilber = 0
+while n>0:
+    niilber = niilber + n
+    # print (n)
+    n=n-1
+print("niilber: " , niilber)
